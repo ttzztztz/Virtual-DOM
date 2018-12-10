050715e7600753c2802c70b4f1180dab4b5f0aa7
ttzztztz <935319340@qq.com>
2018-12-10 13:26:31 +0800

    first commit

---
+++ vdom.js
@@ -0,0 +1,15 @@
+class vDom{
+    constructor(){}
+    static createElement(tagName,props,...children){
+
+    }
+    static diff(tree1,tree2){
+
+    }
+    static patch(tree,diff){
+
+    }
+    static render(tree,node){
+
+    }
+}
